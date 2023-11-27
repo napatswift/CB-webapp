42fbadb95cf6c468c8042977c93afb39dd48f441
napatswift <napat.do@ku.th>
2023-11-28 00:21:24 +0800

    Add styled-components dependency and update CSS styles

--- src/components/StreamRecorder.tsx
+++ src/components/StreamRecorder.tsx
@@ -2,6 +2,7 @@
 
 import { SERVER_BASE_URL } from "@/constants";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import styled from "styled-components";
 
 interface SongAPIResponse {
   results: {
@@ -15,6 +16,22 @@ interface AudioFileCheckerResponse {
     is_valid: boolean;
   };
 }
+
+const SongList = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+const SongItem = styled.div`
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: #ccc;
+  }
+`;
 
 async function audioHandler(
   audioChunks: Blob[]
@@ -44,6 +61,9 @@ async function speechToTextHandler(
   data: {
     text: string;
     timestamp: string;
+    posibleSongs: {
+      name: string;
+    }[];
   };
   error?: string;
 }> {
@@ -67,23 +87,33 @@ async function speechToTextHandler(
 
       return resp.json();
     })
-    .catch((err) => ({ data: { text: "", timestamp: "" } }));
+    .then((data) => {
+      if (data.error) return { error: data.error };
+      return data;
+    })
+    .then(({ data }) => {
+      return {
+        data: {
+          text: data.text,
+          timestamp: data.timestamp,
+          posibleSongs: data.posible_songs,
+        },
+      };
+    });
 }
-
-const FFT_SIZE = 2048;
-const BUFFER_LENGTH = 1024;
-const FILE_HEADER_SIZE = 44;
 
 function StreamAudioRecorder() {
   const mediaRecorder = useRef<MediaRecorder | null>(null);
   const [lyrics, setLyrics] = useState<{ line: string; timestamp: string }[]>(
     []
   );
+  const [possibleSongs, setPossibleSongs] = useState<{ name: string }[]>([]);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const audioChunks = useRef<Blob[]>([]);
   const startRecording = useCallback(() => {
     if (mediaRecorder.current !== null) return;
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      setLyrics([]); // reset lyrics
       mediaRecorder.current = new MediaRecorder(stream, {
         mimeType: "audio/webm;codecs=opus",
       });
@@ -93,29 +123,12 @@ function StreamAudioRecorder() {
       );
 
       mediaRecorder.current.onstop = (e) => {
-        audioHandler([...audioChunks.current]).then((resp) => {
-          console.log("onstop::done", resp.data.is_valid);
-        });
+        mediaRecorder.current = null;
+        audioChunks.current = [];
       };
 
       mediaRecorder.current.ondataavailable = (e) => {
         audioChunks.current.push(e.data);
-
-        /*
-        audioHandler([...audioChunks.current])
-          .then((resp) => {
-            console.log("ondataavailable::whole: done", resp.data.is_valid);
-          })
-          .catch((err) => {
-            setErrorMessage(err.message);
-          });
-
-        /*
-        speechToTextHandler([headerChunk.current, ...audioChunks.current]).then(
-          (resp) => {
-            console.log("ondataavailable::whole: done", resp.data);
-          }
-        );*/
 
         if (audioChunks.current.length < 4) return;
         speechToTextHandler(
@@ -126,24 +139,27 @@ function StreamAudioRecorder() {
           2
         ).then((resp) => {
           if (resp.error) {
-            setErrorMessage((prev) => resp.error || "");
+            setErrorMessage(() => resp.error || "");
             return;
           }
-          if (!resp.data.text) return;
-          setLyrics((prev) =>
-            [
+          if (resp.data.text) {
+            setLyrics((prev) => [
               ...prev,
               { line: resp.data.text, timestamp: resp.data.timestamp },
-            ].sort((a, b) => {
-              if (a.timestamp < b.timestamp) return -1;
-              if (a.timestamp > b.timestamp) return 1;
-              return 0;
-            })
-          );
+            ]);
+          }
+          if (resp.data.posibleSongs) {
+            setPossibleSongs(resp.data.posibleSongs);
+          }
         });
+        audioChunks.current.splice(1, 2); // remove 2 items from index 1
       };
     });
   }, [setLyrics, setErrorMessage]);
+
+  useEffect(() => {
+    console.log("lyrics", lyrics);
+  }, [lyrics]);
 
   const stopRecording = useCallback(() => {
     if (mediaRecorder.current === null) return;
@@ -154,27 +170,38 @@ function StreamAudioRecorder() {
     mediaRecorder.current.stream
       .getAudioTracks()
       .forEach((track) => track.stop());
-    mediaRecorder.current = null;
-    audioChunks.current = [];
   }, []);
 
   return (
-    <div className=" relative w-full">
-      <button className=" absolute top-0 left-0" onClick={startRecording}>
-        Start
-      </button>
-      <button className=" absolute top-0 right-0" onClick={stopRecording}>
-        Stop
-      </button>
+    <div>
       <div>
-        {errorMessage && <div className=" text-red-500">{errorMessage}</div>}
+        <button className=" absolute top-0 left-0" onClick={startRecording}>
+          Start
+        </button>
+        <button className=" absolute top-0 right-0" onClick={stopRecording}>
+          Stop
+        </button>
+        <div>
+          {errorMessage && <div className=" text-red-500">{errorMessage}</div>}
+        </div>
       </div>
-      <div>
-        {lyrics.map((lyric, i) => (
-          <div key={i}>
-            {lyric.timestamp} - {lyric.line}
-          </div>
+      <SongList>
+        {possibleSongs.map((song, i) => (
+          <SongItem key={i}>{song.name}</SongItem>
         ))}
+      </SongList>
+      <div>
+        {lyrics
+          .sort((a, b) => {
+            if (a.timestamp < b.timestamp) return 1;
+            if (a.timestamp > b.timestamp) return -1;
+            return 0;
+          })
+          .map((lyric, i) => (
+            <div key={i}>
+              {lyric.timestamp} - {lyric.line}
+            </div>
+          ))}
       </div>
     </div>
   );
